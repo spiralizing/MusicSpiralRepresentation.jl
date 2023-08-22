5f6e01e3eb541eaaa638ced1d282a9b8b63c3ca7
Documenter.jl <documenter@juliadocs.github.io>
2023-08-22 02:27:15 +0000

    build based on 26e8757

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MusicSpiralRepresentation","category":"page"},{"location":"#MusicSpiralRepresentation","page":"Home","title":"MusicSpiralRepresentation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MusicSpiralRepresentation.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MusicSpiralRepresentation]","category":"page"}]
+}
